f4b9d25f60fe4104d86a06529a1c9bb58c987949
TabachyshynDanylo <tabachyshyn.danylo@gmail.com>
2017-07-22 22:46:11 +0300

    new branch and new syso

--- LastTestPleeease/src/LastTestPleeease/Main.java
+++ LastTestPleeease/src/LastTestPleeease/Main.java
@@ -3,7 +3,7 @@ package LastTestPleeease;
 public class Main {
 
 	public static void main(String[] args) {
-System.out.println("LastTestPleeease");
+System.out.println("You think these is the end? noooo it's only is a beginning!!!");
 	}
 
 }
