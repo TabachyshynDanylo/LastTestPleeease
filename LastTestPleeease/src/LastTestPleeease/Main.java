7a0b9c72d3fbd2d9a6cf7277623eb8fc5e5193b6
TabachyshynDanylo <tabachyshyn.danylo@gmail.com>
2017-07-22 23:06:58 +0300

    new syso

--- LastTestPleeease/src/LastTestPleeease/Main.java
+++ LastTestPleeease/src/LastTestPleeease/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 System.out.println("You think these is the end? noooo it's only is a beginning!!!");
 System.out.println("nyga-nyga");
+System.out.println("yo-yo");
 	}
 
 }
